5a053bae1a565ca1d0ed2f91971863e956c6ef6a
hamidseptian <hamidseptian@gmail.com>
2021-08-07 09:40:23 +0700

    first commit

---
+++ user/admin/form/dashboard/dashboard.php
@@ -0,0 +1,51 @@
+<ul>
+	<li>Di Menu Home
+		<ul>
+			<li>ada kalender Bookingan (kalender asli)</li>
+			<li>Nav Bar (Home, Profile, Bookingan(Ada Kalender), Paket Dan Harga, Belanja, Cara Pesan, Buat Akun, Login (Admin, User), Kontak )</li>
+		</ul>
+	</li>
+	<li>Di Admin
+		<ul>
+			<li>Harga Cetak (ok)</li>
+			<li>Harga Paket (Libatkan Id harga Cetak di 1 Kolom. Lihat yang di diklat bagian penelitian)(ok - paket dan cetak dibuat terpisah)</li>
+			<li>Pemesanan (Online Dan Offline)(ok)</li>
+			<li>Galery (ok)</li>
+			<li>Laporan Pemesanan Dan Penjualan (ok)</li>
+			<li>Bookingan (ada cek booking onlline) - (Online dan offline) (ada jangka waktu dan pembatalan otomatis)</li>
+			<li>Album Foto (ok - diganti dengan history foto)</li>
+			<li>Pelanggan (Online Dan Offline (ok))</li>
+			<li>Rekening(ok)</li>
+
+		</ul>
+	</li>
+</ul>
+
+<hr>
+<h4>Tambahan Aplikasi (Dilakukan setelah revisi pertama penulisan) 20072020</h4>
+<ul>
+	<li>Di menu Home
+		<ul>
+			<li>Nav Bar Acc (Home, Bookingan(Ada Kalender), Paket Dan Cetak, Keranjang, History)</li>
+			<li>Cek kembali detail history cetak dari sisi pembayaran, status order</li>
+			<li>Kalender Booking</li>
+			<li>Sistem Booking (pengecekan jadwal yang sudah ada)</li>
+			<li>Cek history booking</li>
+			<li>Perbaiki pilihan login</li>
+			
+			<li>Bersihkan </li>
+			<li>Edit akun dan ganti password</li>
+
+		</ul>
+	</li>
+	<li>Di Admin
+		<ul>
+			<li>Identias Admin</li>
+			<li>Sistem booking samakan dengan cetak</li>
+			<li>Edit akun dan ganti password</li>
+			<li>Otomatis ganti status jika tgl sekarang sudah melewati tgl selesai pada booking</li>
+			<li>upload foto untuk order cetak (dibuat satu satu saja nanti)</li>
+		</ul>
+	</li>
+
+</ul>
